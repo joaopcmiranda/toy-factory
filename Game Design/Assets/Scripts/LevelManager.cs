f573bab03ef59a0697c5a89b72579c76dff087ed
Joao Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2024-04-01 02:14:24 +1100

    Core Project Files and structure

---
+++ Game Design/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    private int _currentScene;
+    private int _overlayScene;
+
+    private void UnloadCurrentScene()
+    {
+        if (_currentScene != 0)
+        {
+            SceneManager.UnloadSceneAsync(_currentScene);
+        }
+
+        if (_overlayScene != 0)
+        {
+            SceneManager.UnloadSceneAsync(_overlayScene);
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        UnloadCurrentScene();
+
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        _currentScene = 1;
+    }
+
+    public void LoadLevel0()
+    {
+        UnloadCurrentScene();
+
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        _currentScene = 2;
+    }
+
+    public void LoadLevel1()
+    {
+        UnloadCurrentScene();
+
+        SceneManager.LoadScene(3, LoadSceneMode.Additive);
+        _currentScene = 3;
+    }
+
+    public void LoadLevel2()
+    {
+        UnloadCurrentScene();
+
+        SceneManager.LoadScene(4, LoadSceneMode.Additive);
+        _currentScene = 4;
+    }
+
+    public void LoadLevel3()
+    {
+        UnloadCurrentScene();
+
+        SceneManager.LoadScene(5, LoadSceneMode.Additive);
+        _currentScene = 5;
+    }
+
+    public void LoadLevel4()
+    {
+        UnloadCurrentScene();
+
+        SceneManager.LoadScene(6, LoadSceneMode.Additive);
+        _currentScene = 6;
+    }
+
+}
