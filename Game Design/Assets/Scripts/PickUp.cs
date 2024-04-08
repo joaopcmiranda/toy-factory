bcd85c28a75d8eca50ef624d55603d1bd6e08eef
Trible <108388213+Trible903@users.noreply.github.com>
2024-04-08 11:24:55 +1000

    Initial Collision Implementation

---
+++ Game Design/Assets/Scripts/PickUp.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holdSpot;
+    public LayerMask pickUpMask;
+
+    public Vector3 Direction { get; set; }
+    private GameObject itemHolding;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) 
+        {
+            if (itemHolding) 
+            {
+
+            }
+            else 
+            {
+                Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, .4f, pickUpMask);
+                if(pickUpItem) 
+                {
+                    itemHolding = pickUpItem.gameObject;
+                    itemHolding.transform.position = holdSpot.position;
+                    itemHolding.transform.parent = transform;
+                    if (itemHolding.GetComponent<Rigidbody2D>())
+                        itemHolding.GetComponent<Rigidbody2D>().simulated = false; 
+
+                }
+            }
+        }
+    }
+}
