2a02f48d91423d0566454f211cecc07d5d9d6e70
João Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2024-04-09 23:46:42 +1000

    Code cleanup / Machine Manager  (#13)
    Co-authored-by: amezzles <141393246+amezzles@users.noreply.github.com>

---
+++ Game Design/Assets/Scripts/managers/MachineManager.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using machines;
+using UnityEngine;
+
+namespace managers
+{
+    public class MachineManager : MonoBehaviour
+    {
+        public List<GameObject> machines;
+        private readonly List<Tuple<GameObject, Machine>> _machines = new List<Tuple<GameObject, Machine>>();
+
+        private void Start()
+        {
+            foreach (var machine in machines)
+            {
+                _machines.Add(new Tuple<GameObject, Machine>(machine, machine.GetComponent<Machine>()));
+            }
+        }
+
+        private Machine _previouslyHighlightedMachine;
+
+        public Tuple<GameObject, Machine> GetNearestMachineTupleWithinDropRadius(Transform target)
+        {
+            Tuple<GameObject, Machine> nearestMachine = null;
+            var nearestDistance = Mathf.Infinity;
+
+            foreach (var machine in _machines)
+            {
+                var distance = Vector2.Distance(machine.Item1.transform.position, target.position);
+                var machineComponent = machine.Item2;
+
+                if (machineComponent && distance <= machineComponent.dropRadius && distance < nearestDistance)
+                {
+                    nearestMachine = machine;
+                    nearestDistance = distance;
+                }
+            }
+
+            return nearestMachine;
+        }
+
+
+        public Machine HighlightNearestMachineWithinRadius(Transform target)
+        {
+
+            var nearestMachineTuple = GetNearestMachineTupleWithinDropRadius(target);
+            var nearestMachineComponent = nearestMachineTuple?.Item2;
+            var nearestMachine = nearestMachineTuple?.Item1;
+
+
+            if (nearestMachineComponent != _previouslyHighlightedMachine)
+            {
+                if (_previouslyHighlightedMachine)
+                {
+                    _previouslyHighlightedMachine.SetMachineColor(Color.white);
+                }
+
+                if (nearestMachine)
+                {
+                    nearestMachineComponent.SetMachineColor(Color.grey);
+                    _previouslyHighlightedMachine = nearestMachineComponent;
+                }
+                else
+                {
+                    _previouslyHighlightedMachine = null;
+                }
+            }
+            return nearestMachineComponent;
+        }
+
+
+
+    }
+}
---
+++ Game Design/Assets/Scripts/player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        public float speed = 5.0f;
+        private Rigidbody2D _rb;
+        private Vector2 _movement;
+        private Vector2 _lastDirection = Vector2.right;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = Input.GetAxisRaw("Vertical");
+
+            if (_movement != Vector2.zero)
+            {
+                _lastDirection = _movement.normalized; // Update lastDirection when the player moves
+            }
+
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.MovePosition(_rb.position + _movement * (speed * Time.fixedDeltaTime));
+        }
+
+        public Vector2 GetFacingDirection()
+        {
+            return _lastDirection;
+        }
+    }
+}
