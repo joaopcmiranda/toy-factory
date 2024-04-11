61d80b338852de955d564104b4320f31bfc5fb25
abz0 <143391418+abz0@users.noreply.github.com>
2024-04-11 13:47:27 +1000

    Menu screen functionality (#15)
    no graphics, just function

---
+++ Game Design/Assets/Scripts/menu/Menu.cs
@@ -0,0 +1,60 @@
+using machines;
+using managers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace menu
+{
+    public class Menu : MonoBehaviour
+    {
+        private LevelManager level;
+
+        void Start()
+        {
+            level = GameObject.Find("Managers").GetComponent<LevelManager>();
+        }
+
+        void Update() {}
+
+        public void OnStartButton()
+        {
+            level.LoadLevel0();
+        }
+
+        public void OnOptionsButton() {}
+
+        public void OnExitButton() 
+        {
+            //it doesn't work in unity, but probably works in the real game
+            Application.Quit(); 
+        }
+
+        public void OnNextLevelButton()
+        {
+            switch (level.GetLevelScene())
+            {
+                case 0:
+                    level.LoadLevel1();
+                    break;
+                case 1:
+                    level.LoadLevel2();
+                    break;
+                case 2:
+                    level.LoadLevel3();
+                    break;
+                case 3:
+                    level.LoadLevel4();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void OnMenuButton()
+        {
+            level.LoadMainMenu();
+        }
+    }
+}
