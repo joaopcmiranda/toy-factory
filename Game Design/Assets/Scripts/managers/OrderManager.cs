ea060ef458ce2c109205e8457ff37fff62bde820
Trible <108388213+Trible903@users.noreply.github.com>
2024-04-13 21:25:05 +1000

    Implemented basic order system

--- Game Design/Assets/Scripts/managers/OrderManager.cs
+++ Game Design/Assets/Scripts/managers/OrderManager.cs
@@ -4,11 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public struct toy
-{
-    public string tag;
-}
-
 public struct Order
 {
     public string orderName;
@@ -45,21 +40,28 @@ public class OrderManager : MonoBehaviour
     public Text timerTextThree;
     public Text timerTextFour;
 
+    public Text scoreText;
+    public int score; 
+
     // Start is called before the first frame update
     void Start()
     {
-        orders = new Order[4];
-        timerTexts = new Text[4];
+        orders = new Order[1];
+        timerTexts = new Text[1];
+
+        score = 0;
+        scoreText.text = "Score: " + score.ToString(); 
 
         timerTexts[0] = timerTextOne;
-        timerTexts[1] = timerTextTwo;
-        timerTexts[2] = timerTextThree;
-        timerTexts[3] = timerTextFour;
+        //timerTexts[1] = timerTextTwo;
+        //timerTexts[2] = timerTextThree;
+        //timerTexts[3] = timerTextFour;
 
         for (int i = 0; i < orders.Length; i++)
         {
             orders[i] = new Order("Order " + (i + 1), Random.Range(10.0f, 30.0f), timerTexts[i]);
         }
+
     }
 
     // Update is called once per frame
@@ -69,22 +71,43 @@ public class OrderManager : MonoBehaviour
         for (int i = 0; i < orders.Length; i++)
         {
             orders[i].timeLeft -= Time.deltaTime;
-            if (orders[i].isExpired()) ReplaceOrder(i);
+            if (orders[i].isExpired()) CreateNewOrder(i);
 
             orders[i].UpdateTimerText();
         }
         //IF ITEM IS DROPPED ON OUTBOX, CHECK IF IT IS A CURRENT ORDER
     }
 
-    private void ReplaceOrder(int index)
+    public void ReplaceOrder(int index)
     {
-        //SUBTRACT POINTS?
         orders[index] = new Order("New Order", Random.Range(10.0f, 30.0f), timerTexts[index]);
     }
 
-    private void CreateNewOrder(int index)
+    public void CreateNewOrder(int index)
     {
-        //ADD POINTS? 
+        decreaseScore(100);
         orders[index] = new Order("New Order", Random.Range(10.0f, 30.0f), timerTexts[index]);
+    }
+
+    public Order[] getOrders()
+    {
+        return orders; 
+    }
+
+    public void increaseScore(int i)
+    {
+        score += i;
+        updateScore(score);
+    }
+
+    public void decreaseScore(int i)
+    {
+        score -= i;
+        updateScore(score);
+    }
+
+    public void updateScore(int score)
+    {
+        scoreText.text = "Score: " + score.ToString();
     }
 }
