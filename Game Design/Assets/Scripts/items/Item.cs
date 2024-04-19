d7d66d1b81c01b55adf22c306db6ea6fdc01b11c
amezzles <141393246+amezzles@users.noreply.github.com>
2024-04-19 16:00:15 +1000

    items highlight
    but now don't go into the machines

--- Game Design/Assets/Scripts/items/Item.cs
+++ Game Design/Assets/Scripts/items/Item.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using managers;
 
 namespace items
 {
@@ -6,12 +7,33 @@ namespace items
     {
         private Rigidbody2D _rigidbody2D;
         private SpriteRenderer _spriteRenderer;
+        private static ItemManager _itemManager;
         public bool IsHeldByMachine { get; set; }
+
+        private void Awake()
+        {
+            if (_itemManager == null)
+            {
+                _itemManager = FindObjectOfType<ItemManager>();
+                if (_itemManager == null)
+                    Debug.LogError("ItemManager not found in the scene!");
+                else
+                    Debug.Log("ItemManager found and set.");
+            }
+        }
 
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void SetItemColor(Color color)
+        {
+            if (_spriteRenderer)
+            {
+                _spriteRenderer.color = color;
+            }
         }
 
         public void PickUp(Transform holdSpot)
@@ -23,6 +45,7 @@ namespace items
 
             transform.position = holdSpot.position;
             transform.SetParent(holdSpot);
+            _itemManager?.RefreshItems();
         }
 
         public void Drop()
@@ -33,6 +56,7 @@ namespace items
             }
 
             transform.SetParent(null);
+            _itemManager?.RefreshItems();
         }
 
         public void SetSprite(Sprite sprite)
@@ -43,6 +67,7 @@ namespace items
         public void DeleteItem()
         {
             Destroy(gameObject);
+            _itemManager?.RefreshItems();
         }
     }
 }
