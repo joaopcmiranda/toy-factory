9282f3501c9b79dee8f688d2b7fa908f0600bf51
Joao Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2024-04-22 04:24:08 +1000

    Order Queue ready :)

--- Game Design/Assets/Scripts/orders/Order.cs
+++ Game Design/Assets/Scripts/orders/Order.cs
@@ -1,51 +1,86 @@
-using System;
+using System.Collections.Generic;
+using items;
 using recipes;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 public class Order : MonoBehaviour
 {
-    public IRecipe recipe;
+    private IRecipe _recipe;
+
+    public List<Sprite> progressSprites;
+    public SpriteRenderer progressSpriteRenderer;
+    private int _currentSpriteIndex = 0;
+
     public TextMeshProUGUI orderNameText;
     public Canvas itemNameCanvas;
-    [FormerlySerializedAs("ItemSprite")]
     public SpriteRenderer itemSprite;
+    public int queuePosition;
+    public float speed = 0.5f;
+    public ItemType deliveryItem
+    {
+        get
+        {
+            return _recipe.deliveryItem;
+        }
+    }
 
-    private float timeLeft;
-    private OrderManager orderManager;
+    private Transform _transform;
+    private float _timeLeft;
+    private OrderManager _orderManager;
+    private bool _isExpired = false;
 
     public int remainingPoints
     {
         get
         {
-            return (int) (timeLeft / recipe.timeLimit * recipe.points);
+            return (int)(_timeLeft / _recipe.timeLimit * _recipe.points);
         }
     }
 
     private void Start()
     {
-        timeLeft = recipe.timeLimit;
-        orderManager = FindObjectOfType<OrderManager>();
+        _transform = transform;
+        _timeLeft = _recipe.timeLimit;
+        _orderManager = FindObjectOfType<OrderManager>();
+        itemNameCanvas.worldCamera = Camera.main;
         ShowItemName(false);
     }
 
     private void Update()
     {
-        timeLeft -= Time.deltaTime;
-        if (timeLeft <= 0)
+        if (_isExpired)
         {
-            orderManager.OrderExpired(this);
+            return;
+        }
+        var deltaTime = Time.deltaTime;
+        _timeLeft -= deltaTime;
+
+        var currentSpriteIndex = (int)((1 - _timeLeft / _recipe.timeLimit) * progressSprites.Count);
+        if (currentSpriteIndex != _currentSpriteIndex)
+        {
+            _currentSpriteIndex = currentSpriteIndex;
+            progressSpriteRenderer.sprite = progressSprites[currentSpriteIndex];
+        }
+
+        if (_timeLeft <= 0)
+        {
+            _isExpired = true;
+            _orderManager.OrderExpired(this);
+        }
+
+        if (transform.position.x > queuePosition)
+        {
+            _transform.position += Vector3.left * (speed * deltaTime);
         }
     }
 
     public void SetRecipe(IRecipe recipe)
     {
-        this.recipe = recipe;
-        orderNameText.SetText(recipe.recipeName);
-        itemSprite.sprite = recipe.GetIcon();
-        timeLeft = recipe.timeLimit;
+        _recipe = recipe;
+        orderNameText.SetText(_recipe.recipeName);
+        itemSprite.sprite = _recipe.GetIcon();
+        _timeLeft = _recipe.timeLimit;
     }
 
     public void OnMouseOver()
@@ -58,7 +93,7 @@ public class Order : MonoBehaviour
         ShowItemName(false);
     }
 
-    public void ShowItemName(bool show)
+    private void ShowItemName(bool show)
     {
         itemNameCanvas.enabled = show;
     }
--- Game Design/Assets/Scripts/orders/OrderManager.cs
+++ Game Design/Assets/Scripts/orders/OrderManager.cs
@@ -1,33 +1,38 @@
-
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using items;
 using recipes;
 using score;
 using UnityEngine;
-using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class OrderManager : MonoBehaviour
 {
-    private List<Order> orders;
-
-    public Text orderText;
-    public int orderNumber = 1;
+    private readonly List<Order> _orders = new List<Order>();
 
     public GameObject orderPrefab;
     public Transform orderEntryPoint;
+    public bool singleOrderLevel;
 
-    private List<IRecipe> _recipes;
+    private readonly List<IRecipe> _recipes = new List<IRecipe>();
     private ScoreManager _scoreManager;
 
 
-    void Start()
+   public void Start()
     {
         _scoreManager = FindObjectOfType<ScoreManager>();
+        if (singleOrderLevel)
+        {
+            CreateNewOrder();
+        }
+        else
+        {
+            StartOrderCoroutine();
+        }
     }
 
-    public void StartOrderCoroutine()
+    private void StartOrderCoroutine()
     {
         StartCoroutine(LaunchOrderLoop());
     }
@@ -38,11 +43,11 @@ public class OrderManager : MonoBehaviour
         {
             yield return new WaitForSeconds(1); // Wait for 1 second
 
-            CreateNewOrder(GetRecipe());
+            CreateNewOrder();
         }
     }
 
-    private IRecipe GetRecipe()
+    private IRecipe GetRecipe(bool force = false)
     {
         // Recipes in _recipes have a frequency (per minute) property. should pick from the list of recipes based on frequency
         // Example:
@@ -51,7 +56,9 @@ public class OrderManager : MonoBehaviour
         // R3 - 1 ( /60 )
         // Frequencies per call of this function should be 1/60, 2/60, 1/60 and the rest return nothing;
 
-        int random = Random.Range(0, 60);
+        int totalFrequency = _recipes.Sum(recipe => recipe.frequency);
+
+        int random = Random.Range(0, force ? totalFrequency : 60);
         foreach (var recipe in _recipes)
         {
             random -= recipe.frequency;
@@ -64,35 +71,63 @@ public class OrderManager : MonoBehaviour
         return null;
     }
 
-    public void CreateNewOrder(IRecipe recipe)
+    private void CreateNewOrder(bool force = false)
     {
+        var recipe = singleOrderLevel ? _recipes.First() : GetRecipe(force);
+
+        if (recipe == null)
+        {
+            return;
+        }
+
         var orderObj = Instantiate(orderPrefab, orderEntryPoint.position, orderEntryPoint.rotation, transform);
         var order = orderObj.GetComponent<Order>();
         order.SetRecipe(recipe);
-
+        order.queuePosition = _orders.Count;
+        _orders.Add(order);
     }
 
     public bool FinishOrder(Item delivery)
     {
-        foreach (var order in orders)
-        {
-            if (order.recipe.deliveryItem == delivery.type)
-            {
-                _scoreManager.IncreaseScore(order.remainingPoints);
-                orders.Remove(order);
-                Destroy(order.gameObject);
-                return true;
-            }
-        }
+        var order = _orders.Find(order => order.deliveryItem == delivery.type);
 
-        return false;
+        if (!order)
+            return false;
+
+        _scoreManager.IncreaseScore(order.remainingPoints);
+        RemoveOrder(order);
+        return true;
+    }
+
+    private void UpdateItemQueuePosition()
+    {
+        for (int i = 0; i < _orders.Count; i++)
+        {
+            _orders[i].queuePosition = i;
+        }
     }
 
     public void OrderExpired(Order order)
     {
-        orders.Remove(order);
-        Destroy(order.gameObject);
+        RemoveOrder(order);
         _scoreManager.DecreaseScore(100);
+        if (_orders.Count == 0)
+        {
+            CreateNewOrder(true);
+        }
+    }
+
+    private void RemoveOrder(Order order)
+    {
+        _orders.Remove(order);
+        var go = order.gameObject;
+        Destroy(order);
+        Destroy(go);
+        if (_orders.Count == 0)
+        {
+            CreateNewOrder(true);
+        }
+        UpdateItemQueuePosition();
     }
 
     public void RegisterRecipe(IRecipe recipe)
