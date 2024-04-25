ef5a99a95178518ebf049c580ea85f20301c3747
João Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2024-04-26 00:57:55 +1000

    Added different cursors to the game (#45)

---
+++ Game Design/Assets/Scripts/UI/MouseOverCursor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+namespace UI
+{
+    public class MouseOverCursor : MonoBehaviour
+    {
+        public CursorType cursorType = CursorType.Default;
+        private CursorManager _cursorManager;
+
+        private void Start()
+        {
+            _cursorManager = FindObjectOfType<CursorManager>();
+        }
+
+        private void OnMouseEnter()
+        {
+           _cursorManager.SetCursor(cursorType);
+        }
+
+        private void OnMouseExit()
+        {
+            _cursorManager.SetCursor(CursorType.Default);
+        }
+    }
+}
