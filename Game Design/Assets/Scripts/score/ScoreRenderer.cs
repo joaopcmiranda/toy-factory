14c62bfb233882e512a1dbd89902bf102f3f2f49
Trible <108388213+Trible903@users.noreply.github.com>
2024-04-28 13:04:52 +1000

    Implemented star UI

--- Game Design/Assets/Scripts/score/ScoreRenderer.cs
+++ Game Design/Assets/Scripts/score/ScoreRenderer.cs
@@ -1,26 +1,27 @@
 using score;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreRenderer : MonoBehaviour
 {
 
-    SpriteRenderer StarOne, StarTwo, StarThree;
+    public SpriteRenderer StarOne, StarTwo, StarThree;
+    public Sprite starFull, starEmpty;
+    public TextMeshProUGUI scoreText; 
 
     void Start()
     {
         // Retrieve score
         int score = PlayerPrefs.GetInt("PlayerScore");
+        //Reset score
         PlayerPrefs.SetInt("PlayerScore", 0);
 
         StarOne = GameObject.Find("StarOne").GetComponent<SpriteRenderer>();
         StarTwo = GameObject.Find("StarTwo").GetComponent<SpriteRenderer>();
         StarThree = GameObject.Find("StarThree").GetComponent<SpriteRenderer>();
-
-        StarOne.enabled = false;
-        StarTwo.enabled = false;
-        StarThree.enabled = false;
 
         // Render stars based on score
         RenderStars(score);
@@ -28,25 +29,33 @@ public class ScoreRenderer : MonoBehaviour
 
     void RenderStars(int score)
     {
+        scoreText.text = "Score: " + score; 
         if (score < 100)
         {
-            StarOne.enabled = true;
-            StarTwo.enabled = false;
-            StarThree.enabled = false;
-            //render one star
-        } 
+            StarOne.sprite = starEmpty;
+            StarTwo.sprite = starEmpty;
+            StarThree.sprite = starEmpty;
+            //render no stars 
+        }
         else if (score >= 100 && score < 300)
         {
-            StarOne.enabled = true;
-            StarTwo.enabled = true;
-            StarThree.enabled = false;
+            StarOne.sprite = starFull;
+            StarTwo.sprite = starEmpty;
+            StarThree.sprite = starEmpty;
+            //render one star
+        } 
+        else if (score >= 300 && score < 500)
+        {
+            StarOne.sprite = starFull;
+            StarTwo.sprite = starFull;
+            StarThree.sprite = starEmpty;
             //render two stars
         } 
-        else if (score >= 300)
+        else if (score >= 500)
         {
-            StarOne.enabled = true;
-            StarTwo.enabled = true;
-            StarThree.enabled = true;
+            StarOne.sprite = starFull;
+            StarTwo.sprite = starFull;
+            StarThree.sprite = starFull;
             //render three stars
         }
         Debug.Log(score);
