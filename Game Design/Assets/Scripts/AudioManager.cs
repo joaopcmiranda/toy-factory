c410cb1fb77626020cb3351e0e96400b94cff44a
amezzles <141393246+amezzles@users.noreply.github.com>
2024-05-06 09:35:42 +1000

    Tutorial sound effects and music (#54)

---
+++ Game Design/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource soundEffects;
+    public AudioSource music;
+
+    public AudioClip machine;
+    public AudioClip item;
+    public AudioClip order;
+    public AudioClip levelComplete;
+    public AudioClip machineFinished;
+    public AudioClip negativeScore;
+
+    private int currentPriority = 0;
+
+    public void PlayMachine()
+    {
+        PlaySound(machine, 1);
+    }
+
+    public void PlayItem()
+    {
+        PlaySound(item, 1);
+    }
+
+    public void PlayOrder()
+    {
+        PlaySound(order, 2);
+    }
+
+    public void PlayLevelComplete()
+    {
+        PlaySound(levelComplete, 3);
+    }
+
+    public void PlayMachineComplete()
+    {
+        PlaySound(machineFinished, 1);
+    }
+
+    public void PlayNegativeScore()
+    {
+        PlaySound(negativeScore, 2);
+    }
+
+    private void PlaySound(AudioClip clip, int priority)
+    {
+        if (!soundEffects.isPlaying || priority >= currentPriority)
+        {
+            soundEffects.clip = clip;
+            soundEffects.Play();
+            currentPriority = priority;
+        }
+    }
+}
