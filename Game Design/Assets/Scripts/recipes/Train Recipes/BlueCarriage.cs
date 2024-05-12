2fa4782810aabc6b3c55c38a47319ed24d178037
amezzles <141393246+amezzles@users.noreply.github.com>
2024-05-13 02:10:11 +1000

    changes

--- Game Design/Assets/Scripts/recipes/Train Recipes/BlueCarriage.cs
+++ Game Design/Assets/Scripts/recipes/Train Recipes/BlueCarriage.cs
@@ -7,7 +7,7 @@ namespace recipes
     {
         public string recipeName { get; set; } = "Blue Carriage";
         public int frequency { get; set; } = 1;
-        public int timeLimit { get; set; } = 90;
+        public int timeLimit { get; set; } = 120;
         public int points { get; set; } = 200;
         public Sprite trainIcon;
         public ItemType deliveryItem
