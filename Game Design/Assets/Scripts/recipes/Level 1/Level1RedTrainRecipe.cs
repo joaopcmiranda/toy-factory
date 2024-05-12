e26dc1be8b1f837c39cef7f14d88dbc9cc2d4615
abbymailau <abbymailau@gmail.com>
2024-05-13 01:25:28 +1000

    working order system

---
+++ Game Design/Assets/Scripts/recipes/Level 1/Level1RedTrainRecipe.cs
@@ -0,0 +1,31 @@
+using items;
+using managers;
+using UnityEngine;
+namespace recipes
+{
+    public class Level1RedTrainRecipe : MonoBehaviour, IRecipe
+    {
+        public string recipeName { get; set; } = "Plastic Red Train";
+        public int frequency { get; set; } = 20;
+        public int timeLimit { get; set; } = 60;
+        public int points { get; set; } = 100;
+        public Sprite trainIcon;
+        public ItemType deliveryItem
+        {
+            get
+            {
+                return ItemType.RedTrain;
+            }
+        }
+
+        public Sprite GetIcon()
+        {
+            return trainIcon;
+        }
+
+        private void Awake()
+        {
+            gameObject.GetComponent<OrderManager>().RegisterRecipe(this);
+        }
+    }
+}
