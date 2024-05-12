65653144176a305aeb46afb25f109c5e82142ba8
Trible <108388213+Trible903@users.noreply.github.com>
2024-05-13 08:29:23 +1000

    Completed (#77)

--- Game Design/Assets/Scripts/menu/OrderBookButton.cs
+++ Game Design/Assets/Scripts/menu/OrderBookButton.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,12 +8,29 @@ public class OrderBookButton : MonoBehaviour
     public Image orderBook;
     public Image secondaryOrder;
 
+    public Text Disclaimer;
+    public Text Assembly;
+    public Text Carving;
+    public Text Chopping;
+    public Text Painting;
+
+    public Image background; 
+
     private bool bookOpen = false;
 
     // Start is called before the first frame update
     void Start()
     {
         orderBook.enabled = false;
+        Disclaimer.enabled = false;
+        Assembly.enabled = false;
+        Carving.enabled = false;
+        Chopping.enabled = false;
+        Painting.enabled = false;
+
+        background.enabled = false;
+
+
         if (secondaryOrder)
         {
             secondaryOrder.enabled = false;
@@ -23,6 +41,15 @@ public class OrderBookButton : MonoBehaviour
     {
         bookOpen = !bookOpen;
         orderBook.enabled = bookOpen;
+        Disclaimer.enabled = bookOpen;
+        Assembly.enabled = bookOpen;
+        Carving.enabled = bookOpen;
+        Chopping.enabled = bookOpen;
+        Painting.enabled = bookOpen;
+
+        background.enabled = bookOpen;
+
+
         if (secondaryOrder)
         {
             secondaryOrder.enabled = bookOpen;
