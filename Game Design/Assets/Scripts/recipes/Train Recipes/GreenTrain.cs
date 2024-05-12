7790fb498d855919584c6781cc8972ba685587db
amezzles <141393246+amezzles@users.noreply.github.com>
2024-05-13 01:28:52 +1000

    level fully functional!

--- Game Design/Assets/Scripts/recipes/Train Recipes/GreenTrain.cs
+++ Game Design/Assets/Scripts/recipes/Train Recipes/GreenTrain.cs
@@ -7,8 +7,8 @@ namespace recipes
     {
         public string recipeName { get; set; } = "Green Train";
         public int frequency { get; set; } = 1;
-        public int timeLimit { get; set; } = 60;
-        public int points { get; set; } = 100;
+        public int timeLimit { get; set; } = 90;
+        public int points { get; set; } = 200;
         public Sprite trainIcon;
         public ItemType deliveryItem
         {
