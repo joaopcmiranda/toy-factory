bfd92dc80b680c6c55a99dd7af1895dbbaeb0a33
João Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2024-05-12 16:06:30 +1000

    [WIP] A lot of stuff (#63)
    General code improvements, 
    highlighting rework,
    keyboard interactions,
     improvements on the order belt, 
    color of order changes as time passes
    pick up drop off rework, 
    machine rework, 
    tables, 
    multi character play,
    code organisation,
    Item holder/provider/receiver
    Items use type not Tag now
    2 Player Movement
    Player Movement Bug
    
    ---------
    
    Co-authored-by: amezzles <141393246+amezzles@users.noreply.github.com>

---
+++ Game Design/Assets/Scripts/stations/PaintingStation.cs
@@ -0,0 +1,76 @@
+using items;
+using items.handling;
+
+namespace stations
+{
+    public class PaintingStation : ItemHolder
+    {
+        public Timer timer;
+        private bool _paintLoaded;
+
+
+        public override Item GetItem()
+        {
+            if (!timer.IsTimeUp())
+            {
+                timer.ResetTimer();
+            }
+            return ReleaseLastItem();
+        }
+
+        public override Item PutItem(Item item)
+        {
+            if (!CanReceiveItem(item)) return item;
+            Item returnItem;
+            if (item.type == ItemType.Paint)
+            {
+                _paintLoaded = true;
+                item.DeleteItem();
+                returnItem = null;
+            }
+            else
+            {
+                returnItem = HoldItem(item);
+            }
+
+            if (_paintLoaded && IsHoldingItem())
+            {
+                timer.StartTimer(3);
+            }
+
+            return returnItem;
+        }
+
+        public override bool CanReceiveItem(Item item)
+        {
+            switch (item.type)
+            {
+                case ItemType.Paint:
+                case ItemType.UnpaintedTrainParts:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private void Update()
+        {
+            if (timer.IsTimeUp() && timer.IsActive() && _paintLoaded && IsHoldingItem())
+            {
+                Transform();
+                timer.ResetTimer();
+            }
+        }
+
+
+        private void Transform()
+        {
+            ReleaseLastItem()
+                ?.DeleteItem();
+
+            var item = itemManager.CreateItem(ItemType.PaintedTrainParts, transform);
+            HoldItem(item);
+            _paintLoaded = false;
+        }
+    }
+}
