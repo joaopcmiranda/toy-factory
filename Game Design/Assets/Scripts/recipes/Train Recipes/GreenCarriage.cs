31e40831088aea1554b34e9c48ffdf9e78d53b5b
amezzles <141393246+amezzles@users.noreply.github.com>
2024-05-13 02:57:57 +1000

    Level 2 fully functional (just bug testing to be done) (#67)
    Co-authored-by: Joao Miranda <50178167+joaopcmiranda@users.noreply.github.com>

---
+++ Game Design/Assets/Scripts/recipes/Train Recipes/GreenCarriage.cs
@@ -0,0 +1,28 @@
+using items;
+using managers;
+using UnityEngine;
+namespace recipes
+{
+    public class GreenCarriage : MonoBehaviour, IRecipe
+    {
+        public string recipeName { get; set; } = "Green Carriage";
+        public int frequency { get; set; } = 1;
+        public int timeLimit { get; set; } = 120;
+        public int points { get; set; } = 200;
+        public Sprite trainIcon;
+        public ItemType deliveryItem
+        {
+            get => ItemType.GreenCarriage;
+        }
+
+        public Sprite GetIcon()
+        {
+            return trainIcon;
+        }
+
+        private void Awake()
+        {
+            gameObject.GetComponent<OrderManager>().RegisterRecipe(this);
+        }
+    }
+}
